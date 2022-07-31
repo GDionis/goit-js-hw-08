585148b3bf7f6557b0012eeaba0234c3f7699e11
Denys <zpdeni10@gmail.com>
2022-07-31 20:19:18 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,65 +1,19 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
-// console.log(galleryItems);
-
+import { galleryItems } from '../js/gallery-items';
+import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.min.css";
 function createGal(array) {
-  return array.reduce((acc, item) =>  acc + `<div class="gallery__item">
-  <a class="gallery__link" href="${item.original}">
-    <img
-      class="gallery__image"
-      src="${item.preview}"
-      data-source="${item.original}"
-      alt="${item.description}"
-    />
-  </a>
-</div>`, ''
+  return array.reduce((acc, item) =>  acc + `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`, ''
   );
 }
 
 const result = createGal(galleryItems);
 
-const gallery = document.querySelector('.gallery');
+let gallery = document.querySelector('.gallery');
 gallery.insertAdjacentHTML('beforeend', result);
 
-let instance;
-function clickImg(evt) {
-  evt.preventDefault()
-  if (!evt.target.classList.contains('gallery__image')) {
-    return
-  }
-  instance = basicLightbox.create(`
-    <img src="${evt.target.dataset.source}" >
-`,
-    {
-      onShow: instance => {
-        window.addEventListener('keydown', escapeKeydown);
-      },
-      onClose: instance => {
-        window.removeEventListener('keydown', escapeKeydown);
-      },
-    }
-  );
-  instance.show((instance) => console.log('finished show()', instance));
-
-  // setTimeout(() => {
-  //   instance.close((instance) => console.log('finished close()', instance))
-  // }, 3000);
-}
-
-gallery.addEventListener('click', clickImg);
-
-function escapeKeydown(event) {
-  const esc = 'Escape';
-  if (event.code === esc) {
-    instance.close()
-  }
-}
-
-
-// Відмалювати розмітку галереї використовуючи масив
-// При рендері галереї потрібно підставити в розмітку правильні поля
-// Потрібно навішати клік на галерею використовуючи делегування
-// Визначити елемент на який клікнути через event.target
-// Считати данні з атрибутом data-source і підставити в модальне вікно
-// Має зявитися модальне вікно з картинкою яку отримали з атрибута data-source 
+ gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
